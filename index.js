a4a2e4a7f3e2302a75d8039432ec87dc336e3276
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-05-03 15:32:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 class Formatter {
   //add static methods here
+  
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '')
+  }
+
+  static titleize(sentence) {
+    let except= ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let result = [];
+    let array = sentence.split(" ");
+
+    for (let i = 0; i < array.length; i++) {
+      if (i === 0) {
+        result.push(this.capitalize(array[i]))
+      } else {
+        if (except.includes(array[i])) {
+          result.push(array[i])
+        } else {
+          result.push(this.capitalize(array[i]))
+        }
+      }
+    }
+    return result.join(" ");
+  }
 }
