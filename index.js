63fcbf17ff499a6a0a19b05570333b64c06e9c10
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-06-14 19:43:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+  
+  static sanitize(str) {
+    str = str.replace(/[~!@#$%^&*()_+={}]/gim,"");
+    return str.trim();
+  }
+  
+  static titleize(string) {
+     var string_array = string.split(' ');
+    string_array = string_array.map(function(str,i) {
+       if(i != 0 && ["the", "a", "an", "but", "of", "and", "for", "at", "by","from"].includes(str)) return str;
+       else return str.charAt(0).toUpperCase() + str.slice(1);
+    });
+    
+    return string_array.join(' ');
+	    
+  }
 }
